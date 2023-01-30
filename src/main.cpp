035f1fab7bfb08cb742fd2f9aedccb9279920ba9
TEVIN LEWIS <TML22D@FSU.EDU>
2023-01-29 20:17:49 -0500

    corrected code placement

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
-
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int reading  = analogRead(A0);
+  Serial.println(reading);
+  delay(1000);
 }
